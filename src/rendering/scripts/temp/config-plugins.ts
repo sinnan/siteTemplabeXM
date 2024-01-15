01eb19b6137869c31d33e3840cd124f1109487ec
sinnan <sinnan.imrash@gmail.com>
2024-01-15 11:51:56 +0400

    Initial commit

---
+++ src/rendering/scripts/temp/config-plugins.ts
@@ -0,0 +1,6 @@
+export { computedPlugin } from 'scripts/config/plugins/computed';
+export { fallbackPlugin } from 'scripts/config/plugins/fallback';
+export { multisitePlugin } from 'scripts/config/plugins/multisite';
+export { packageJsonPlugin } from 'scripts/config/plugins/package-json';
+export { scjssconfigPlugin } from 'scripts/config/plugins/scjssconfig';
+export { sxaPlugin } from 'scripts/config/plugins/sxa';
